ee3daa4a12b3d3dc24ebf68b5f8ef14e4713d130
yongho <yongho73@gmail.com>
2018-05-26 15:20:52 +0900

    t1

--- src/kosta/hello.java
+++ src/kosta/hello.java
@@ -3,6 +3,7 @@ package kosta;
 public class hello {
 	public static void main(String[] args) {
 		System.out.println("hello world");
-		System.out.println("hello world");
+		System.out.println("hello world  ");
+		System.out.println("topic ");
 	}
 }
